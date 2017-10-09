caf367f3723d52c33ee0e6208f34c50a4be5de12
萧鸣 <boois@qq.com>
2017-10-10 02:54:59 +0800

    -

---
+++ Example/Pods/Target Support Files/TestPodsFramework/TestPodsFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TestPodsFramework : NSObject
+@end
+@implementation PodsDummy_TestPodsFramework
+@end
---
+++ Example/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  TestPodsFramework
+//
+//  Created by boois on 2017/10/10.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+
+class TestClass {
+
+    public var TestName:String = ""
+    
+}
